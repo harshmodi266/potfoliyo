edff9c8fb04022d58070f1c3c5b8bef9ed68c1a2
Harsh Modi <harshmodi266@gmail.com>
2026-02-19 18:07:09 +0530

    Added PHPMailer and updated contact form

---
+++ Portfolio-Website-main/contact.php
@@ -0,0 +1,48 @@
+<?php
+header('Content-Type: application/json');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$response = ["status" => "error"];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'harshmodi084@gmail.com';
+        $mail->Password   = 'tcuqjonmsfqkbqdp';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+        $mail->setFrom('harshmodi084@gmail.com', 'Portfolio Contact');
+        $mail->addReplyTo($_POST['email'], $_POST['name']);
+        $mail->addAddress('harshmodi084@gmail.com');
+
+        $mail->Subject = "New Contact Message";
+        $mail->Body = "
+        Name: {$_POST['name']}
+        Email: {$_POST['email']}
+        Phone: {$_POST['phone']}
+        Message: {$_POST['message']}
+        ";
+
+        $mail->send();
+
+        $response["status"] = "success";
+
+    } catch (Exception $e) {
+        $response["message"] = $mail->ErrorInfo;
+    }
+}
+
+echo json_encode($response);
+exit;
